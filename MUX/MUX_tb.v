470ddecfbf94e278e715eced7e79f3e02807121d
João Pedro da Silva Nascimento <jpsn2@cin.ufpe.br>
2022-05-11 22:37:54 -0300

    TestBench e comprimento de onda do MUX

--- MUX/MUX_tb.v
+++ MUX/MUX_tb.v
@@ -3,25 +3,27 @@ module MUX_tb;
     reg A_tb, B_tb, S_tb;
     wire X_tb;
  
-    MUX1 dut( A_tb, B_tb, S_tb, X_tb);
+    MUX dut( A_tb, B_tb, S_tb, X_tb);
  
-    initial
-    begin
-    A_tb = 1'b0; B_tb = 1'b0; S_tb = 1'b0; #1
-    $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
-    A_tb = 1'b0; B_tb = 1'b1; S_tb = 1'b0; #1
-    $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
-    _tb = 1'b1; B_tb = 1'b0; S_tb = 1'b0; #1
-    $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
-    A_tb = 1'b1; B_tb = 1'b1; S_tb = 1'b0; #1
-    $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
-    A_tb = 1'b0; B_tb = 1'b0; S_tb = 1'b1; #1
-    $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
-    A_tb = 1'b0; B_tb = 1'b1; S_tb = 1'b1; #1
-    $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
-    A_tb = 1'b1; B_tb = 1'b0; S_tb = 1'b1; #1
-    $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
-    A_tb = 1'b1; B_tb = 1'b1; S_tb = 1'b1; #1
-    $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
+    initial begin
+        $dumpfile("MUX_tb.vcd");
+        $dumpvars(0, MUX_tb);
+
+        A_tb = 1'b0; B_tb = 1'b0; S_tb = 1'b0; #1
+        $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
+        A_tb = 1'b0; B_tb = 1'b1; S_tb = 1'b0; #1
+        $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
+        A_tb = 1'b1; B_tb = 1'b0; S_tb = 1'b0; #1
+        $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
+        A_tb = 1'b1; B_tb = 1'b1; S_tb = 1'b0; #1
+        $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
+        A_tb = 1'b0; B_tb = 1'b0; S_tb = 1'b1; #1
+        $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
+        A_tb = 1'b0; B_tb = 1'b1; S_tb = 1'b1; #1
+        $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
+        A_tb = 1'b1; B_tb = 1'b0; S_tb = 1'b1; #1
+        $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
+        A_tb = 1'b1; B_tb = 1'b1; S_tb = 1'b1; #1
+        $display("A = %x, B = %x, S = %x, X = %x", A_tb, B_tb, S_tb, X_tb);
     end
 endmodule
