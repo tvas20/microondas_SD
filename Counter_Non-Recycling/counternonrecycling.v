c2d810864a26ab0be40eb297557f20cb0351bea3
João Pedro da Silva Nascimento <jpsn2@cin.ufpe.br>
2022-05-11 23:29:40 -0300

    Em andamento a construnção do test bench do contador não reciclável

--- Counter_Non-Recycling/counternonrecycling.v
+++ Counter_Non-Recycling/counternonrecycling.v
@@ -1,17 +1,13 @@
 module counternonrecycling (clk, rst, q);
     input clk, rst;
-    output [3:0] q;
-    reg [3:0] count;
+    output reg [3:0] q;
 
-    always @(posedge clk or negedge rst)
-    begin
-      if (rst == 1’b0)
-      count <= 4’b0000;
-      else if (count == 4’b0111)
-      count <= 4’b0000;
+    always @(posedge clk or negedge rst) begin
+      if (rst == 1'b0)
+      q <= 4'b0000;
+      else if (q == 4'b0111)
+      q <= 4'b0000;
       else
-      count <= count + 1;
+      q <= q + 1;
     end
-
-    assign q = count;
 endmodule
