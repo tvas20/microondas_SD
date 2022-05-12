ba4d4b6380ae933237d3dc100765cd86d8113c7b
João Pedro da Silva Nascimento <jpsn2@cin.ufpe.br>
2022-05-11 23:07:10 -0300

    Atualização doa algoritmos do contador não reciclável

--- Counter_Non-Recycling/counternonrecycling.v
+++ Counter_Non-Recycling/counternonrecycling.v
@@ -1,9 +1,17 @@
-module counternonrecycling (input clock, output reg [7:0] q);
-    initial begin
-      q = 8'b00000000;
+module counternonrecycling (clk, rst, q);
+    input clk, rst;
+    output [3:0] q;
+    reg [3:0] count;
+
+    always @(posedge clk or negedge rst)
+    begin
+      if (rst == 1’b0)
+      count <= 4’b0000;
+      else if (count == 4’b0111)
+      count <= 4’b0000;
+      else
+      count <= count + 1;
     end
 
-    always @(posedge clock) begin
-        q = q + 8'b00000001;
-    end
+    assign q = count;
 endmodule
