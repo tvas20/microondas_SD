b10f16bce584cc3db5e75e323e117760826ae263
Tiago Victor de Amorim Silva <61971708+tvas20@users.noreply.github.com>
2022-05-12 21:17:50 -0300

    Update encoder.v

--- Timer_input/Codificador/encoder.v
+++ Timer_input/Codificador/encoder.v
@@ -3,33 +3,49 @@ input wire [9:0]A, input wire en);
 always @ (A, en)
 begin
     //recebimento do timer
-    if (A==1'b1 & en == 0)
+    if (A==1'b1 & en == 0)begin
          B = 4'b0000;//0
-    else if (A==2'b10 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==2'b10 & en == 0)begin
          B = 4'b0001; //1
-    else if (A==3'b100 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==3'b100 & en == 0)begin
          B = 4'b0010; //2
-    else if (A==4'b1000 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==4'b1000 & en == 0)begin
          B = 4'b0011; //3
-    else if (A==5'b10000 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==5'b10000 & en == 0)begin
          B = 4'b0100; //4
-    else if (A==6'b100000 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==6'b100000 & en == 0)begin
          B = 4'b0101; //5
-    else if (A==7'b1000000 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==7'b1000000 & en == 0)begin
          B = 4'b0110; //6
-    else if (A==8'b10000000 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==8'b10000000 & en == 0)begin
          B = 4'b0111; //7
-    else if (A==9'b100000000 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==9'b100000000 & en == 0)begin
          B = 4'b1000; //8
-    else if (A==10'b1000000000 & en == 0)
+         dv = 1'b0;
+         end
+    else if (A==10'b1000000000 & en == 0)begin
          B = 4'b1001; //9
-    else
-         B = 4'b1111;
-         
-    //checagem da entrada valida 
-    if (B == 4'b1111)
-        dv = 1'b1;
-    else 
-        dv = 1'b0;
+         dv = 1'b0;
+         end
+    else begin
+         B = 4'bXXXX;
+         dv = 1'b1; 
+         end
 end   
 endmodule
